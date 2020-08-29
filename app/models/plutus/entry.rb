71410533d68aa5a329d91cdf19da562af3bce751
nicolasrsande <nicolasrsande@gmail.com>
2020-08-29 12:09:21 -0300

    Destroy Amounts when entry is destroyed and change some syntax

--- app/models/plutus/entry.rb
+++ app/models/plutus/entry.rb
@@ -25,15 +25,23 @@ module Plutus
     before_save :default_date
 
     if ActiveRecord::VERSION::MAJOR > 4
-      belongs_to :commercial_document, :polymorphic => true, optional: true
+      belongs_to :commercial_document, polymorphic: true, optional: true
     else
-      belongs_to :commercial_document, :polymorphic => true
+      belongs_to :commercial_document, polymorphic: true
     end
 
-    has_many :credit_amounts, :extend => AmountsExtension, :class_name => 'Plutus::CreditAmount', :inverse_of => :entry
-    has_many :debit_amounts, :extend => AmountsExtension, :class_name => 'Plutus::DebitAmount', :inverse_of => :entry
-    has_many :credit_accounts, :through => :credit_amounts, :source => :account, :class_name => 'Plutus::Account'
-    has_many :debit_accounts, :through => :debit_amounts, :source => :account, :class_name => 'Plutus::Account'
+    has_many :credit_amounts, extend: AmountsExtension, class_name: 'Plutus::CreditAmount', inverse_of: :entry
+    has_many :debit_amounts, extend: AmountsExtension, class_name: 'Plutus::DebitAmount', inverse_of: :entry
+    has_many :credit_accounts,
+             through: :credit_amounts,
+             source: :account,
+             class_name: 'Plutus::Account',
+             dependent: :destroy
+    has_many :debit_accounts,
+             through: :debit_amounts,
+             source: :account,
+             class_name: 'Plutus::Account',
+             dependent: :destroy
 
     validates_presence_of :description
     validate :has_credit_amounts?
@@ -57,21 +65,22 @@ module Plutus
     end
 
     private
-      def default_date
-        todays_date = ActiveRecord::Base.default_timezone == :utc ? Time.now.utc : Time.now
-        self.date ||= todays_date
-      end
+    
+    def default_date
+      todays_date = ActiveRecord::Base.default_timezone == :utc ? Time.now.utc : Time.now
+      self.date ||= todays_date
+    end
 
-      def has_credit_amounts?
-        errors[:base] << "Entry must have at least one credit amount" if self.credit_amounts.blank?
-      end
+    def has_credit_amounts?
+      errors[:base] << 'Entry must have at least one credit amount' if self.credit_amounts.blank?
+    end
 
-      def has_debit_amounts?
-        errors[:base] << "Entry must have at least one debit amount" if self.debit_amounts.blank?
-      end
+    def has_debit_amounts?
+      errors[:base] << 'Entry must have at least one debit amount' if self.debit_amounts.blank?
+    end
 
-      def amounts_cancel?
-        errors[:base] << "The credit and debit amounts are not equal" if credit_amounts.balance_for_new_record != debit_amounts.balance_for_new_record
-      end
+    def amounts_cancel?
+      errors[:base] << 'The credit and debit amounts are not equal' if credit_amounts.balance_for_new_record != debit_amounts.balance_for_new_record
+    end
   end
 end
